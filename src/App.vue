95651994111aebab415263720bff3d46a690939e
Dmitry Dmitriev <asd.dmitriev2014@yandex.ru>
2024-04-12 22:06:22 +0700

    add theme switcher

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,10 @@ setInterval(() => (date.value = setTime()), 1000 * 60);
 					<img src="/src/icons/exit-icon.svg" alt="#" />
 				</button>
 			</div>
+			<label class="switch">
+				<input type="checkbox" />
+				<span class="slider"></span>
+			</label>
 		</div>
 		<div class="main"></div>
 	</div>
@@ -41,6 +45,8 @@ setInterval(() => (date.value = setTime()), 1000 * 60);
 	border-top-right-radius: 16px;
 	border-bottom-right-radius: 16px;
 	box-shadow: 6px 0px 7px rgba(0, 0, 0, 0.3);
+	display: flex;
+	flex-direction: column;
 }
 
 .main {
@@ -80,5 +86,54 @@ setInterval(() => (date.value = setTime()), 1000 * 60);
 	font-size: 24px;
 	color: #ffffff;
 	padding-right: 10px;
+}
+
+.switch {
+	position: relative;
+	display: inline-block;
+	width: 76px;
+	height: 20px;
+	align-self: flex-end;
+	margin-top: 88px;
+	margin-right: 40px;
+}
+
+.switch input {
+	display: none;
+}
+
+.slider {
+	position: absolute;
+	cursor: pointer;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	background-color: #586c92;
+	-webkit-transition: 0.4s;
+	transition: 0.4s;
+	border-radius: 12px;
+}
+
+.slider:before {
+	position: absolute;
+	content: '';
+	height: 71px;
+	width: 71px;
+	left: -25px;
+	bottom: -21px;
+	background-image: url(/src/icons/theme-switcher.svg);
+	-webkit-transition: 0.5s;
+	transition: 0.5s;
+}
+
+input:checked + .slider {
+	background-color: #000;
+}
+
+input:checked + .slider:before {
+	-webkit-transform: translateX(50px);
+	-ms-transform: translateX(50px);
+	transform: translateX(50px);
 }
 </style>
